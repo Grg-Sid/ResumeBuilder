86286a8a27ecfe0a1cff2efae0e9395221d96a13
Grg-Sid <106266279+Grg-Sid@users.noreply.github.com>
2023-01-21 04:02:56 +0530

    Update views.py

--- resumeBuilder/views.py
+++ resumeBuilder/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 
 
 def gpt3(stext):
-    openai.api_key = "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"
+    openai.api_key = "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"
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt="generate a sentence for resume using the word %s without telling the the time and should not start in first person" % stext,
